958b930e2de7a365b78dbbeafec29db8b35147de
abhayMore <abhay7more@gmail.com>
2020-03-19 22:46:38 +0530

    updated template, added simple sprite to check

--- main.cpp
+++ main.cpp
@@ -1,19 +1,36 @@
 #include <SFML/Graphics.hpp>
-
+#include <iostream>
+using namespace std;
 int main()
 {
 
-
-
 	sf::RenderWindow window(sf::VideoMode(600,600),"SCREEN");
+
+	sf::Texture texture;
+	if(!texture.loadFromFile("texture.jpeg"))
+	{
+		cout<<"Error loading file"<<endl;
+	}
+		
+	sf::Sprite sprite(texture);
+	
 	while(window.isOpen())
 	{
-		sf::Event evnt;
-		while(window.pollEvent(evnt))
+		sf::Event event;
+		while(window.pollEvent(event))
 		{
-			if(evnt.type == evnt.Closed)
-				window.close();	
-		}	
+			switch (event.type)
+			{
+				case sf::Event::Closed:
+					window.close();
+					break;
+			}	
+		}
+		window.clear();
+		
+		window.draw(sprite);
+
+		window.display();	
 	}	
 	
 	return 0;
