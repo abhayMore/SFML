7d111b0956bb960ada09ac88e302c188772dbd6f
abhayMore <49201569+abhayMore@users.noreply.github.com>
2020-10-10 18:54:37 +0530

    Code Refactoring

--- main.cpp
+++ main.cpp
@@ -49,10 +49,10 @@ class Player
 {
 private:
 	sf::RectangleShape m_Rectangle;
-	const float m_RectSizeWidth		= 20.0f;
+	const float m_RectSizeWidth	= 20.0f;
 	const float m_RectSizeHeight	= 20.0f;
-	const int m_RectPositionX		= 0;
-	const int m_RectPositionY		= 0;
+	const int m_RectPositionX	= 0;
+	const int m_RectPositionY	= 0;
 	float m_Speed;
 	float m_MoveX;
 	float m_MoveY;
