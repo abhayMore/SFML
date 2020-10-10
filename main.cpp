c8746a3c842fda94feb4c86a2af01fe08f3386c4
abhayMore <abhay7more@gmail.com>
2020-10-10 18:52:26 +0530

    Added Player class

--- main.cpp
+++ main.cpp
@@ -45,32 +45,82 @@ Config GetConfigData()
 	return LocalData;
 }
 
+class Player
+{
+private:
+	sf::RectangleShape m_Rectangle;
+	const float m_RectSizeWidth		= 20.0f;
+	const float m_RectSizeHeight	= 20.0f;
+	const int m_RectPositionX		= 0;
+	const int m_RectPositionY		= 0;
+	float m_Speed;
+	float m_MoveX;
+	float m_MoveY;
+
+public:
+	Player()
+	{
+		m_Speed	= 20;
+		m_MoveX = 0;
+		m_MoveY	= 0;
+		m_Rectangle.setSize(sf::Vector2f(m_RectSizeWidth, m_RectSizeHeight));
+		m_Rectangle.setFillColor(RED);
+		m_Rectangle.setPosition(m_RectPositionX,m_RectPositionY);
+	}
+
+	void draw( sf::RenderWindow& Window)
+	{
+		Window.draw(m_Rectangle);
+	}
+
+	void onKeyDown(sf::Keyboard::Key& KeyCode)
+	{
+		switch(KeyCode)
+		{
+			case sf::Keyboard::Right:
+			{
+				m_MoveX += m_Speed;
+			}
+			break;
+			case sf::Keyboard::Left:
+			{
+				m_MoveX += -m_Speed;
+			}
+			break;
+			case sf::Keyboard::Up:
+			{
+				m_MoveY += -m_Speed;
+			}
+			break;
+			case sf::Keyboard::Down:
+			{
+				m_MoveY += m_Speed;
+			}
+			break;
+		}
+	}
+
+	void onKeyUp(sf::Keyboard::Key& KeyCode)
+	{
+	}
+
+	void update()
+	{
+		m_Rectangle.setPosition(m_MoveX, m_MoveY);
+	}
+};
+
 int main()
 {
 	const  Config Data(GetConfigData());
 
 	sf::RenderWindow Window(sf::VideoMode(Data.WINDOW_WIDTH, Data.WINDOW_HEIGHT), Data.WINDOW_TITLE);
 
-
-	sf::RectangleShape Rectangle;
-	const float RectSizeWidth  = 20.0f;
-	const float RectSizeHeight = 20.0f;
-	const int RectPositionX	   = 0;
-	const int RectPositionY	   = 0;
-
-	Rectangle.setSize(sf::Vector2f(RectSizeWidth, RectSizeHeight));
-	Rectangle.setFillColor(RED);
-	Rectangle.setPosition(RectPositionX,RectPositionY);
-
-	float Speed = 10.0f;
-	float MoveX =  0.0f;
-	float MoveY =  0.0f;
+	Player Snake;
 
 	while(Window.isOpen())
 	{
 		sf::Event GameEvent;
-
-
 
 		while(Window.pollEvent(GameEvent))
 		{
@@ -84,41 +134,24 @@ int main()
 
 				case sf::Event::KeyPressed:
 				{
-					switch(GameEvent.key.code)
-					{
-						case sf::Keyboard::Right:
-						{
-							MoveX += Speed;
-						}
-						break;
-						case sf::Keyboard::Left:
-						{
-							MoveX += -Speed;
-						}
-						break;
-						case sf::Keyboard::Up:
-						{
-							MoveY += -Speed;
-						}
-						break;
-						case sf::Keyboard::Down:
-						{
-							MoveY += Speed;
-						}
-						break;
-					}
+					Snake.onKeyDown(GameEvent.key.code);
+				}
+				break;
+
+				case sf::Event::KeyReleased:
+				{
+					Snake.onKeyUp(GameEvent.key.code);
 				}
 				break;
 			}
 
-
-			Rectangle.setPosition(MoveX, MoveY);
+			Snake.update();
 
 
 
 			Window.clear();
 
-			Window.draw(Rectangle);
+			Snake.draw(Window);
 
 			Window.display();
 		}
