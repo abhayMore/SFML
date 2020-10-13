ca1a89f6c6d4c4fa9ce5696cf20c288e524124d6
akshayMore2018 <37394442+akshayMore2018@users.noreply.github.com>
2020-10-13 11:24:59 +0530

    Code review
    Added comments

--- Fruit.cpp
+++ Fruit.cpp
@@ -8,12 +8,12 @@
 Fruit::Fruit()
 {
   srand(time(NULL));
-  x =(rand() % 30)*m_FruitSizeWidth;
-  y = (rand() % 30)*m_FruitSizeHeight;
+  x =(rand() % 30)*m_FruitSizeWidth;//I want a position variable for fruit. If I want to get Fruit's position somewhere else, make it so that I can retrieve it through object of class fruit.
+  y = (rand() % 30)*m_FruitSizeHeight; // with current setup there is no way of getting that outside this class and even if it was I would have to do it through m_Fruit which means making m_Fruit public which I dont want at the moment.
   m_Fruit.setSize(sf::Vector2f(m_FruitSizeWidth, m_FruitSizeHeight));
   m_Fruit.setFillColor(GREEN);
   m_Fruit.setOrigin(m_Fruit.getSize().x/2, m_Fruit.getSize().y/2);
-  m_Fruit.setPosition(x,y);
+  m_Fruit.setPosition(x,y); 
 }
 
 void Fruit::draw(sf::RenderWindow& Window)
@@ -23,4 +23,5 @@ void Fruit::draw(sf::RenderWindow& Window)
 
 void Fruit::update()
 {
+  //missed the setposition here. 
 }
